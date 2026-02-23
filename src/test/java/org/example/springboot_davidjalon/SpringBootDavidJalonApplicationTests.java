9269fd60be0da66019956cf847856bac59379b38
davidjalon1992 <davidjalon@protonmail.com>
2026-02-23 18:28:08 +0100

    init commit

---
+++ src/test/java/org/example/springboot_davidjalon/SpringBootDavidJalonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot_davidjalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDavidJalonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
