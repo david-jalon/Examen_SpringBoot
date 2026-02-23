a2aba45bff9fe6ffee33ea0b1624d7b017b9e00c
davidjalon1992 <davidjalon@protonmail.com>
2026-02-23 19:10:42 +0100

    final commit

--- src/main/java/org/example/springboot_davidjalon/repository/ProductoRepository.java
+++ src/main/java/org/example/springboot_davidjalon/repository/ProductoRepository.java
@@ -2,6 +2,16 @@ package org.example.springboot_davidjalon.repository;
 
 import org.example.springboot_davidjalon.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    @Query("SELECT p.nombre FROM Producto p WHERE p.categoria = :category")
+    List<Producto> findByCategoria(@Param("category") String category);
+
+    @Query("SELECT p.nombre FROM Producto p WHERE p.stock >= :filtro")
+    List<String> filtrar(@Param("filtro") Integer filtro);
 }
--- src/main/java/org/example/springboot_davidjalon/services/ProductoService.java
+++ src/main/java/org/example/springboot_davidjalon/services/ProductoService.java
@@ -1,13 +1,33 @@
 package org.example.springboot_davidjalon.services;
 
+import org.example.springboot_davidjalon.entities.Producto;
 import org.example.springboot_davidjalon.repository.ProductoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductoService {
-    ProductoRepository ProductoRepository;
+    ProductoRepository productoRepository;
     public ProductoService(ProductoRepository productoRepository) {
-        this.ProductoRepository = productoRepository;
+        this.productoRepository = productoRepository;
     }
 
+
+    public List<Producto> findAll() {
+        return productoRepository.findAll();
+    }
+
+    public Producto findById(Long id) {
+        return productoRepository.findById(id).get();
+    }
+
+    public List<Producto> findByCategoria(String category) {
+        return productoRepository.findByCategoria(category);
+    }
+
+    public List<String> fitrar(Integer filtro) {
+        return productoRepository.filtrar(filtro);
+    }
 }
